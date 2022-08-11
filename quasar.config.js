41726962f74234e8405eaaa0606d10c209d6259f
Ebert Zerpa <ebertunerg@gmail.com>
2022-08-11 12:02:13 -0500

    Despliegue en Pages

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: 'https://ebertlast.github.io/quasar-curso/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
