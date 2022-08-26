2f073b4d2ea9899809c1cf453e8402058d58e39e
Ebert Zerpa <ebertunerg@gmail.com>
2022-08-25 22:20:45 -0500

    Deploy

---
+++ docs/assets/IndexPage.a564ad85.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.7733aa18.js";import{_ as a,j as t,k as o,l as s,m as n,C as r}from"./index.b48f70d8.js";const c=t({name:"IndexPage"}),_=r(" Index Page ");function d(f,p,x,i,l,m){return o(),s(e,{class:"flex flex-center"},{default:n(()=>[_]),_:1})}var P=a(c,[["render",d]]);export{P as default};
